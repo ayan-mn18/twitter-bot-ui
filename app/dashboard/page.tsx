37c9ae56665cd6d49e415a7d9d6cbd5182517929
ayan-mn18 <ayanmansuri2001@gmail.com>
2024-11-22 23:53:39 +0530

    Added aceternity & shadcn

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+    <div> Form for platform creds </div>
+    <div> Form for job details</div>
+    <div> Activate/Deactivate toggle </div>
+    </>
+  );
+}
