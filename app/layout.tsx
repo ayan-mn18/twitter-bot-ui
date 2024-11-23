5b16adcf62e5af19d409e4fd39f0d4292234a08a
ayan-mn18 <ayanmansuri2001@gmail.com>
2024-11-24 01:43:56 +0530

    build issues resolved

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-        {children}
+            {children}
           </ThemeProvider>
       </body>
     </html>
