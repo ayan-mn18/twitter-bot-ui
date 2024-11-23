799c424253a8334389edbdbcadd8a0f7ac8e0868
ayan-mn18 <ayanmansuri2001@gmail.com>
2024-11-24 02:04:34 +0530

    Resolving build issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,17 @@
 'use client'
 import Hero from "@/components/hero";
 import Navbar from "@/components/navbar";
-import { Switch } from "@/components/ui/switch";
-// import { Spotlight } from "@/components/ui/spotlight";
+import { Spotlight } from "@/components/ui/spotlight";
 
 
 export default function Home() {
   return (
     <>
     <Navbar />
-    {/* <Spotlight
+    <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
-      /> */}
-      <Switch />
+      />
     <Hero />
     </>
   );
